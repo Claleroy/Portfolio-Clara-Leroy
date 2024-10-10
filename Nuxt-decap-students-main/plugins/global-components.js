3f01eeac893fbfb36339896086781c81f81d1c1f
Claleroy <claaaleroy@gmail.com>
2024-10-10 16:06:41 +0200

    Delete Nuxt-decap-students-main directory

--- Nuxt-decap-students-main/plugins/global-components.js
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app';
-import SearchBox from '../components/SearchBox.vue';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('SearchBox', SearchBox);
-});
